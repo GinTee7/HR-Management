fbbea667bed14ccacb01bdfa42f969627e614eff
Trung Tin <duongtrungtinst127@gmail.com>
2024-12-31 15:58:32 +0700

    add vite

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+/ @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./index.html",
+    "./src//*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -88,4 +88,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
