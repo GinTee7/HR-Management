3916f67fcf8858e2df9813a4c68c265fe6f41362
Trung Tin <duongtrungtinst127@gmail.com>
2025-05-14 19:43:31 +0700

    Add sale tax and return order

---
+++ src/types/return-order.ts
@@ -0,0 +1,31 @@
+// Define types for the return order data
+export interface ReturnRequestImage {
+    returnRequestImageId: number
+    imageUrl: string
+}
+
+export interface ReturnRequestDetail {
+    returnRequestDetailId: string
+    orderDetailId: string
+    productName: string
+    reason: string
+    quantityReturned: number
+    images: ReturnRequestImage[]
+}
+
+export interface ReturnRequest {
+
+
+
+    returnRequestId: string
+    orderId: string
+    orderCode: string
+    returnRequestCode: string
+    createdAt: string
+    createdByUserName: string
+    status: string
+    note: string
+    details: ReturnRequestDetail[]
+}
+
+export type SortDirection = "asc" | "desc"
