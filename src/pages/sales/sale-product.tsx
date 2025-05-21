2487c1101d79680bb38b04da8641745be0811dc4
Ducbii4202 <144377938+Ducbii4202@users.noreply.github.com>
2025-05-21 23:18:37 +0700

    index router, sale product page and fix Ui profile saler

--- src/pages/sales/sale-product.tsx
+++ src/pages/sales/sale-product.tsx
@@ -1,7 +1,6 @@
 import { SalesLayout } from "@/layouts/sale-layout"
 import { useState } from "react"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
 import ProductList from "@/components/sales/ProductList"
 import CategoryList from "@/components/sales/CategoryList"
 const SalesProducts = () => {
@@ -15,28 +14,20 @@ const SalesProducts = () => {
                 </div>
                 <Tabs defaultValue="products" value={activeTab} onValueChange={setActiveTab} className="w-full">
                     <div className="bg-white py-2">
-                        <div className="flex justify-between  items-center m-6">
+                        <div className="flex justify-between  items-center mb-2">
                             <TabsList>
                                 <TabsTrigger value="products">Sản phẩm</TabsTrigger>
                                 <TabsTrigger value="categories">Danh mục</TabsTrigger>
                             </TabsList>
                         </div>
-                        <CardHeader>
-                            <CardTitle>{activeTab === "products" ? "Danh sách sản phẩm" : "Danh sách danh mục"}</CardTitle>
-                            <CardDescription>
-                                {activeTab === "products"
-                                    ? "Xem và quản lý tất cả sản phẩm trong hệ thống"
-                                    : "Xem và quản lý các danh mục sản phẩm"}
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent>
+                        <div>
                             <TabsContent value="products" className="mt-0">
                                 <ProductList />
                             </TabsContent>
                             <TabsContent value="categories" className="mt-0">
                                 <CategoryList />
                             </TabsContent>
-                        </CardContent>
+                        </div>
                     </div>
                 </Tabs>
 
